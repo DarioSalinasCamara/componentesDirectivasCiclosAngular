4776a38c035ce33bc325b7a88a2b67c08f1aab02
DarioSalinasCamara <dariosalinascamara@gmail.com>
2024-09-10 21:57:18 +0200

    Rutas hijas, parametros y ciclos de vida del componente, COMMIT FINAL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { usuarioRoutes } from './components/usuario/usuario.routes';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'usuario', component: UsuarioComponent },
+  { path: 'usuario/:id',
+    component: UsuarioComponent,
+    children: usuarioRoutes
+  },
   { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
--- src/app/components/usuario/usuario-nuevo.component.ts
+++ src/app/components/usuario/usuario-nuevo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-nuevo',
@@ -10,5 +11,19 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class UsuarioNuevoComponent {
+
+  constructor( private router: ActivatedRoute ) {
+
+    this.router.params.subscribe(params => {
+      console.log("Ruta hija usuario nuevo");
+      console.log(params);
+    })
+
+    this.router.parent.params.subscribe(params => {
+      console.log("Ruta hija, param padre");
+      console.log(params);
+    })
+
+ }
 
 }
